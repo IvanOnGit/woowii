603bebf5dd30151c17b0dc2491d2d4a55a9a6d54
Fernando <fegwoowii@gmail.com>
2025-02-25 12:20:08 +0100

    onehundred page

---
+++ src/components/CompanyOneHundred/OneHundred.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom"
+import { ButtonNextSecondPage, ContainerWrapper, DivContainerButton, DivContainerLogo, ImgFondoSecondPage, LogoSecondPageStyled } from "./styles"
+
+export default function OneHundred() {
+  return (
+    <>
+    <ContainerWrapper>
+      <DivContainerLogo>
+          <LogoSecondPageStyled src="/images/LogoBlue.svg" />
+      </DivContainerLogo>
+      <ImgFondoSecondPage src="/images/firstwiibuck.svg" alt="Fondo Wiibuck" />
+      <DivContainerButton>
+          <Link to={"/ThirdPage"}>
+              <ButtonNextSecondPage>
+                  Continuar
+              </ButtonNextSecondPage>
+          </Link>
+      </DivContainerButton>
+  </ContainerWrapper>
+</>
+  )
+}
