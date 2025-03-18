41022a667bc53932ff3b63c774d2ffc070612f2d
Ivan Santamans <ivansantamans@gmail.com>
2025-03-18 16:03:54 -0300

    new post now has user profile picture

--- src/components/NewPost/NewPost.tsx
+++ src/components/NewPost/NewPost.tsx
@@ -1,12 +1,29 @@
 import { Smile, Image, Calendar, Plus, Clock } from "lucide-react";
 import { NewPostContainer, Avatar, InputContainer, Actions, PublishButton, Container } from "./styles";
+import { useEffect, useState } from "react";
 
 export default function NewPost() {
+    const userId = localStorage.getItem("userId");
+    const [userData, setUserData] = useState<{ username: string; profile_picture: string } | null>(null);
+
+    useEffect(() => {
+          const fetchUserData = async () => {
+              if (!userId) return;
+    
+              const response = await fetch(`http://localhost:3000/api/auth/get-user?userId=${userId}`);
+              const data = await response.json();
+              console.log(data);  // Verifica qué datos estás recibiendo
+              setUserData(data);
+          };
+    
+          fetchUserData();
+        }, [userId]);
+
     return (
         <>
         <Container>
             <Avatar>
-                <img src="/images/Avatar6.png" alt="User Avatar" />
+            <img src={userData?.profile_picture || "/images/Avatar1.png"} alt="Avatar" />
             </Avatar>
             <NewPostContainer>
                 <InputContainer>
