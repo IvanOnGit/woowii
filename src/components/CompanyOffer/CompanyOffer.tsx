179c56318ae762f1d01f883a69a9c01119fc96a3
Ivan Santamans <ivansantamans@gmail.com>
2025-04-08 10:36:59 -0300

    all forms added

--- src/components/CompanyOffer/CompanyOffer.tsx
+++ src/components/CompanyOffer/CompanyOffer.tsx
@@ -1,7 +1,53 @@
 import { Bell, Bookmark, BriefcaseBusiness, Mail} from "lucide-react";
-import { BaseQuestions, BaseQuestionsInputs, BaseQuestionsInputsFirstContainer, BaseQuestionsInputsSecondContainer, Container, Header, HeaderItems, Logo, SearchBar } from "./styles";
+import { BaseQuestions, BaseQuestionsInputs, BaseQuestionsInputsFirstContainer, BaseQuestionsInputsSecondContainer, Circle, ConnectingLine, Container, FourthContainer, Header, HeaderItems, Logo, ProcessStepsContainer, ResponsibilitiesContainer, ResponsibilityButton, ResponsibilityInput, ResponsibilityInputGroup, Reward, SearchBar, SecondContainer, SkillsContainer, StepInput, StepLineContainer, StepNumber, StepWrapper, TextContainer, ThirdContainer, ThirdContainerColumnsContainer, WhatWeLookingForContainer } from "./styles";
+import { useState } from "react";
 
 export default function CompanyOffer() {
+
+    const [selectedSkills, setSelectedSkills] = useState<string[]>([]);
+
+    const skills = [
+      "React",
+      "Node.js",
+      "Figma",
+      "UX Writing",
+      "Python",
+      "SQL",
+      "Illustrator",
+      "Firebase",
+      "Docker",
+      "Photoshop",
+      "CSS",
+      "HTML",
+      "TypeScript",
+      "Power BI"
+    ];
+
+    const steps = [
+        { reward: "50 Wiibucts", number: 1, active: true },
+        { reward: "100 Wiibucts", number: 2 },
+        { reward: "200 Wiibucts", number: 3 },
+        { reward: "50 Wiibucts", number: 4 },
+    ];
+    
+    const [responsibilities, setResponsibilities] = useState<string[]>(['']);
+
+    const handleResponsibilityChange = (index: number, value: string) => {
+    const updated = [...responsibilities];
+    updated[index] = value;
+    setResponsibilities(updated);
+    };
+
+    const addResponsibility = () => {
+    if (responsibilities[responsibilities.length - 1].trim() !== '') {
+        setResponsibilities([...responsibilities, '']);
+    }
+    };
+
+    const removeResponsibility = (index: number) => {
+    const updated = responsibilities.filter((_, i) => i !== index);
+    setResponsibilities(updated);
+    };
 
     return (
         <>
@@ -49,9 +95,125 @@ export default function CompanyOffer() {
                 </BaseQuestionsInputs>
             </BaseQuestions>
         </Container>
-        <div>
+        <SecondContainer>
             <h1>Que harás</h1>
-        </div>
+            <textarea name="" id="" placeholder="Describe el día a día del rol que estás buscando"></textarea>
+        </SecondContainer>
+        <ThirdContainer>
+            <h2>Con quién trabajarás</h2>
+            <ThirdContainerColumnsContainer>
+                <div className="Column">
+                    <label htmlFor="">Líder</label>
+                    <input type="text" name="" id="" placeholder="Puesto"/>
+                </div>
+                <p>Equipo</p>
+                <div className="Column">
+                    <label htmlFor="">Rol del equipo</label>
+                    <input type="text" name="" id="" placeholder="Descripción"/>
+                </div>
+            </ThirdContainerColumnsContainer>
+        </ThirdContainer>
+        <SkillsContainer>
+            <h2>El kit de supervivencia que necesitarás</h2>
+            <div>
+                <select
+                    onChange={(e) => {
+                    const value = e.target.value;
+                    if (value && !selectedSkills.includes(value)) {
+                        setSelectedSkills([...selectedSkills, value]);
+                    }
+                    }}
+                    defaultValue=""
+                >
+                    <option value="" disabled>Seleccioná una habilidad</option>
+                    {skills.map((skill) => (
+                    <option key={skill} value={skill}>
+                        {skill}
+                    </option>
+                    ))}
+                </select>
+
+                <div className="tags-container">
+                    {selectedSkills.map((skill, index) => (
+                    <span key={index} className="tag">
+                        {skill}
+                    </span>
+                    ))}
+                </div>
+                </div>
+            </SkillsContainer>
+            <FourthContainer>
+                <h2>Nuestro proceso de selección</h2>
+            <ProcessStepsContainer>
+                {steps.map((step, index) => (
+                    <StepWrapper key={index}>
+                    <Reward>{step.reward}</Reward>
+                    <StepLineContainer>
+                        <Circle>
+                        <StepNumber>{step.number}</StepNumber>
+                        </Circle>
+                        {index < steps.length - 1 && <ConnectingLine />}
+                    </StepLineContainer>
+                    <StepInput type="text" placeholder="Describe el proceso.." />
+                    </StepWrapper>
+                ))}
+            </ProcessStepsContainer>
+            </FourthContainer>
+            <ResponsibilitiesContainer>
+                <h2>Responsabilidades</h2>
+                {responsibilities.map((resp, index) => (
+                    <ResponsibilityInputGroup key={index}>
+                    <ResponsibilityInput
+                        type="text"
+                        placeholder="Describe las responsabilidades que tendrá el candidato..."
+                        value={resp}
+                        onChange={(e) => handleResponsibilityChange(index, e.target.value)}
+                    />
+                    {index === responsibilities.length - 1 ? (
+                        <ResponsibilityButton onClick={addResponsibility}>＋</ResponsibilityButton>
+                    ) : (
+                        <ResponsibilityButton onClick={() => removeResponsibility(index)}>－</ResponsibilityButton>
+                    )}
+                    </ResponsibilityInputGroup>
+                ))}
+            </ResponsibilitiesContainer>
+            <WhatWeLookingForContainer>
+                <h2>Qué buscamos</h2>
+                <div className="Needs-Container">
+                    <div>
+                    <h3>Imprescindible</h3>
+                        <div className="Needs-Columns">
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                        </div>
+                    </div>
+                    <div>
+                    <h3>Ideal</h3>
+                        <div className="Needs-Columns">
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                        </div>
+                    </div>
+                    <div>
+                    <h3>Es un plus</h3>
+                        <div className="Needs-Columns">
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                            <input type="text" name="" id="" placeholder="Completar" />
+                        </div>
+                    </div>
+                </div>
+            </WhatWeLookingForContainer>
+            <TextContainer>
+                <h2>Si haz llegado hasta aquí es que algo en la oferta te ah hecho click</h2>
+                <h3>Hagamos match y comentanos qué te motiva para unirte para nuestro equipo.</h3>
+                <button>Publicar solicitud</button>
+            </TextContainer>
         </>
   );
 }
--- src/components/CompanyOffer/styles.ts
+++ src/components/CompanyOffer/styles.ts
@@ -140,3 +140,277 @@ export const BaseQuestionsInputsSecondContainer = styled.div`
     resize: none;
   }
 `;
+
+export const SecondContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+
+  textarea {
+    padding: 1rem;
+    height: 10rem;
+    width: 40rem;
+    border-radius: 1rem;
+    border: 1px solid black;
+    background-color: transparent;
+    color: black; 
+    resize: none;
+  }
+`;
+
+export const ThirdContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5rem;
+
+  p {
+    text-align: center;
+  }
+
+  .Column {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    padding: 1rem;
+  }
+
+  .Column:first-of-type {
+    gap: 4.5rem;
+  }
+`;
+
+export const ThirdContainerColumnsContainer = styled.div`
+`;
+
+export const SkillsContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  .skills-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    max-width: 600px;
+  }
+
+  select {
+    padding: 0.5rem;
+    border-radius: 0.5rem;
+    margin-bottom: 1rem;
+    width: 100%; // para que tenga mismo ancho que las tags
+    max-width: 300px;
+    text-align: center;
+  }
+
+  .tags-container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 0.5rem;
+    width: 100%;
+  }
+
+  .tag {
+    background-color: #4f46e5;
+    color: white;
+    padding: 0.6rem 1rem;
+    border-radius: 2rem;
+    text-align: center;
+    font-size: 0.9rem;
+    font-weight: 500;
+  }
+`;
+
+export const FourthContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ProcessStepsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+  width: 100%;
+  gap: 2rem; /* Espacio uniforme entre pasos */
+`;
+
+export const StepWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 150px; /* Ancho fijo para cada paso */
+`;
+
+export const Reward = styled.div`
+  font-size: 1rem;
+  color: #4f46e5;
+  font-weight: 500;
+  margin-bottom: 1rem;
+  text-align: center;
+  width: 100%;
+`;
+
+export const StepLineContainer = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  position: relative;
+  margin-left: 6rem;
+`;
+
+export const Circle = styled.div`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 50%;
+  background-color: white;
+  border: 4px solid #4f46e5;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`;
+
+export const ConnectingLine = styled.div`
+  position: absolute;
+  height: 3px;
+  background-color: #e0e0e0;
+  margin-left: 2rem;
+  width: 100%;
+  z-index: 1;
+`;
+
+export const StepNumber = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  color: #4f46e5;
+`;
+
+export const StepInput = styled.input`
+  padding: 8px 12px;
+  border: 1px solid #e0e0e0;
+  border-radius: 4px;
+  width: 100%;
+  text-align: center;
+  color: #888;
+  font-size: 14px;
+  margin-top: 1rem;
+  
+  &::placeholder {
+    color: #888;
+  }
+`;
+
+export const ResponsibilitiesContainer = styled.div`
+  margin-top: 40px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  h2 {
+    font-size: 24px;
+    margin-bottom: 16px;
+  }
+`;
+
+export const ResponsibilityInputGroup = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 12px;
+`;
+
+export const ResponsibilityInput = styled.input`
+  flex: 1;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  font-size: 14px;
+  width: 22.5rem;
+  text-align: center;
+`;
+
+export const ResponsibilityButton = styled.button`
+  margin-left: 12px;
+  background-color: transparent;
+  border: none;
+  font-size: 20px;
+  cursor: pointer;
+  color: #6a0dad;
+
+  &:hover {
+    transform: scale(1.2);
+  }
+`;
+
+export const WhatWeLookingForContainer = styled.div`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .Needs-Container {
+    display: flex;
+    gap: 4rem;
+
+    h3 {
+      text-align: center;
+    }
+
+      .Needs-Columns {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        border: 1px solid black;
+        border-radius: 1rem;
+        padding: 1rem;
+
+        input {
+          border: none;
+          border-bottom: 1px solid black;
+      }
+    }
+  }
+`;
+
+export const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 4rem;
+  margin-bottom: 4rem;
+  gap: 1.5rem;
+
+  h2 {
+    margin: 0;
+    padding: 0;
+  }
+
+  h3 {
+    margin: 0;
+    padding: 0;
+  }
+
+  button {
+    background-color: #4f46e5;
+    color: white;
+    padding: 1rem;
+    border-radius: 2rem;
+    border: none;
+    cursor: pointer;
+    font-size: 1.2rem;
+    font-weight: 500;
+  }
+`;
