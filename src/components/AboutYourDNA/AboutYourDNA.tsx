080942dc6875ed87431a5672c8a0d8948aeefd75
Ivan Santamans <ivansantamans@gmail.com>
2025-05-17 15:28:58 -0300

    about your dna page mobile responsiveness added but still lacks hamburger menu

--- src/components/AboutYourDNA/AboutYourDNA.tsx
+++ src/components/AboutYourDNA/AboutYourDNA.tsx
@@ -293,7 +293,7 @@ export default function Hardset() {
                     </SecondDropdownButton>
                   </SecondDropdownContainer>
                   <HelpButton> Chat de ayuda</HelpButton>
-          </MenuAside>
+      </MenuAside>
     <MainContainer>
       <ContainerWrapper>
         <Header>
